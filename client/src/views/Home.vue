93dc676dd6f41bc18adf20ad1bb6334009c53c57
yqcznb <485737827@qq.com>
2019-12-25 08:22:56 +0800

    开发前台页面

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="home">home</div>
+  <div class="home">
+    <van-list
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+    <van-cell
+      v-for="item in list"
+      :key="item"
+      :title="item"
+    />
+    </van-list>
+  </div>
 </template>
 <script>
 export default {
