7d14997e6a9eeb3990e0aa89dd529e2cd06d987d
yqcznb <485737827@qq.com>
2019-12-25 08:29:34 +0800

    开发前台页面

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -9,13 +9,39 @@
     <van-cell
       v-for="item in list"
       :key="item"
-      :title="item"
-    />
+    >
+    <div class="list"></div>
+    </van-cell>
     </van-list>
   </div>
 </template>
 <script>
 export default {
-  name: " home"
+  name: " home",
+  data() {
+    return {
+      list: [],
+      loading: false,
+      finished: false
+    };
+  },
+
+  methods: {
+    onLoad() {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1);
+        }
+        // 加载状态结束
+        this.loading = false;
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true;
+        }
+      }, 500);
+    }
+  }
 };
 </script>
