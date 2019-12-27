2fdd15f16139513edacf7a5c959a2774e83c5124
yqcznb <485737827@qq.com>
2019-12-27 11:03:45 +0800

    moment处理时间问题

--- server/app/controller/article.js
+++ server/app/controller/article.js
@@ -1,9 +1,13 @@
 const Controller = require('egg').Controller;
-
+const moment = require('moment');
 class ArticleController extends Controller {
    async create(){
     const { ctx } = this;
-    const result = await ctx.service.article.create(ctx.request.body);
+    const params = {
+        ...ctx.request.body,
+        createTime: moment().format('YYYY-MM-DD HH:mm:ss')
+    }
+    const result = await ctx.service.article.create(params);
     if(result){
         ctx.body = {
             status: 200,
